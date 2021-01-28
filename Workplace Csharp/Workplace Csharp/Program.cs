cf2149c4da4bb09c1bf279261a8cfedb93115162
mbkev12 <56396880+mbkev12@users.noreply.github.com>
2021-01-28 20:48:47 +0100

    Add files via upload

---
+++ Workplace Csharp/Workplace Csharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Workplace_Csharp
+{
+    public class Multiplication
+    {
+        public int Multiply(int Num1, int Num2, int Num3)
+        {
+            return Num1 + Num2 + Num3;
+        }
+        public int Multiply(int Num1, int Num2)
+        {
+            return Num1 + Num2;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiplication dataClass = new Multiplication();
+            int add2 = dataClass.Multiply(45, 34, 67);
+            int add1 = dataClass.Multiply(23, 34);
+        }
+    }
+
+}
+
